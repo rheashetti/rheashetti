5f7932f156b01b8b2561afde4320ba412f3a6bb3
Rhea Shetti <shettir@uci.edu>
2025-02-05 14:34:03 -0800

    added file styling to experience section

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useState } from 'react';
 import './experience.css';
 
 const Experience : React.FC = () => {
+    const [activeTab, setActiveTab] = useState('pinterest-engage');
 
     return (
         <section id="experience">
@@ -9,8 +11,26 @@ const Experience : React.FC = () => {
             <div className="experience-content">
                 <div className="rounded-container">
                     <div className="file-container">
-                        <button className="file-button">Pinterest Engage</button>
-                        <button className="file-button">WiCS</button>
+                        <button 
+                            className={`file-button ${activeTab === "pinterest-engage" ? "active" : ""}`}
+                            onClick={() => setActiveTab("pinterest-engage")}
+                        >
+                            Pinterest Engage
+                        </button>
+                        <button 
+                            className={`file-button ${activeTab === "wics" ? "active" : ""}`}
+                            onClick={() => setActiveTab("wics")}
+                        >
+                            Women in Information & Computer Sciences
+                        </button>
+                    </div>
+                    <div className={`file-content ${activeTab}`}>
+                        {activeTab === "pinterest-engage" ? (
+                            <p> stuff about pinterst engage </p>
+                        ) : (
+                            <p> stuff about wics </p>
+                        )
+                        }
                     </div>
                 </div>
             </div>
