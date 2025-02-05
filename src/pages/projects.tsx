2441a6a5b797ddbee35e78f1598dc434d2eff00c
Rhea Shetti <shettir@uci.edu>
2025-02-05 14:57:18 -0800

    added projects container

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import './projects.css';
 
 const Projects: React.FC = () => {
 
     return (
         <section id="projects">
             <h2> / projects</h2>
-            <p> this is projects</p>
+            <div className="project-container">
+                <div className="project-item">
+                    <h3>MediTracker</h3>
+                    <p>Project description</p>
+                    <a href="Link">Link to Devpost</a>
+                </div>
+                <div className="project-item">
+                    <h3>PetrPlanner</h3>
+                    <p>Project description</p>
+                    <a href="Link">Link to Devpost</a>
+                </div>
+            </div>
         </section>
     )
 
