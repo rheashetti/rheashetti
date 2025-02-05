a5e7367b9e7f9e77ab757bd45c520085ef45e9df
Rhea Shetti <shettir@uci.edu>
2025-02-04 22:29:18 -0800

    added container to experience

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import './experience.css';
 
 const Experience : React.FC = () => {
 
     return (
         <section id="experience">
             <h2> / experience</h2>
-            <p> this is experience</p>
+            <div className="experience-content">
+                <div className="rounded-container">
+                    <div className="file-container">
+                        <button className="file-button">Pinterest Engage</button>
+                        <button className="file-button">WiCS</button>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 
