0ae31af7d32fddbed23ed3eee099e88042d29879
Rhea Shetti <shettir@uci.edu>
2025-02-09 14:36:18 -0800

    added intro to home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
+import './home.css';
 
 const Home: React.FC = () => {
 
@@ -13,7 +14,7 @@ const Home: React.FC = () => {
             speed={10}
             style={{ color: 'rgb(255, 209, 209)', fontSize: '2em' }}
         />
-        <h4>  yap </h4>
+        <h4>  I'm Rhea, a second-year Computer Science student at UC Irvine with a passion for full-stack development. </h4>
     </section>
   );
 };
